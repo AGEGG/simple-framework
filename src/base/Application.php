ce2e27e324c3224cbe0d351717cfcdc551b1576c
AGEGG <2250389620@qq.com>
2019-06-04 16:25:05 +0800

    v0.3

--- src/base/Application.php
+++ src/base/Application.php
@@ -5,8 +5,17 @@ use Exception;
 
 abstract class Application
 {
+    /**
+     * @var string the namespace that controller classes are located in.
+     * This namespace will be used to load controller classes by prepending it to the controller class name.
+     * The default namespace is `app\controllers`.
+     */
     public $controllerNamespace = 'app\\controllers';
 
+    /**
+     * Runs the application.
+     * This is the main entrance of an application.
+     */
     public function run()
     {
         try{
